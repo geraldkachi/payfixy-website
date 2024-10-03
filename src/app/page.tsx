642fcb440a7b2ae7fda957ec2babacd68397e6cb
geraldkachi <fitzgeraldkachi@gmail.com>
2024-10-03 13:55:40 +0100

    company scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,47 @@ import Button from "@/components/Button";
 import Navbar from "@/components/Navbar";
 import { navVariants, slideIn } from "@/utils/motion";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
+
+type CardType = {
+  url: string;
+  title: string;
+  id: number;
+};
+
+const cards: CardType[] = [
+  {
+    url: "/icons/access.svg",
+    title: "Title 2",
+    id: 2,
+  },
+  {
+    url: "/icons/paycenter.svg",
+    title: "Title 3",
+    id: 3,
+  },
+  {
+    url: "/icons/flutter.svg",
+    title: "Title 4",
+    id: 4,
+  },
+  {
+    url: "/icons/network-solutions.svg",
+    title: "Title 4",
+    id: 5,
+  },
+  {
+    url: "/icons/nibss.svg",
+    title: "Title 4",
+    id: 6,
+  },
+  {
+    url: "/icons/netpost.svg",
+    title: "Title 4",
+    id: 6,
+  },
+];
 export default function Home() {
   return (
     <div className="">
@@ -20,7 +60,7 @@ export default function Home() {
               Effortless Payments, Anywhere, Anytime
             </motion.h1>
             <motion.p variants={slideIn("left", "tween", 0.1, 1)} initial="hidden" whileInView="show"
-            className="text-lg font-medium leading-[24px] tracking-[-1px] text-left text-gray-103 text-[#71737E]">
+              className="text-lg font-medium leading-[24px] tracking-[-1px] text-left text-gray-103 text-[#71737E]">
               Power your business with fast, secure, and reliable payments across borders. Join thousands of businesses using PayFixy to scale seamlessly
             </motion.p>
             <div className="w-full mt-4 md:block">
@@ -38,6 +78,54 @@ export default function Home() {
         </div>
 
       </header>
+
+
+
+      <div className="max-w-[1120px] mx-auto">
+        <div className="relative flex overflow-x-hidden group group-hover:pause">
+          <div className="py-12 sm:py-0 animate-marquee whitespace-nowrap flex items-center gap-[48px] group group-hover:pause">
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[0].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[1].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[2].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[3].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[4].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[5].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+          </div>
+
+          <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap flex items-center gap-[48px] group group-hover:pause">
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[0].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[1].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[2].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[3].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[4].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+            <span className="mx-4 text-4xl w-[120px]">
+              <Image src={cards[5].url} width={100} height={100} alt="scroll-image" className="h-full" />
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
