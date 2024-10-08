b85b562704dabfe71e2c6c030826efa129e74d43
geraldkachi <fitzgeraldkachi@gmail.com>
2024-10-08 16:49:55 +0100

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,15 +7,15 @@ const Footer = () => {
         <div>
             <footer className="bg-white">
                 {/* <SubFooter /> */}
-                <div className="mx-auto w-full max-w-5xl">
-                    <div className="md:flex justify-between w-full gap-4">
+                <div className="mx-auto w-full max-w-[1129px]">
+                    <div className="md:flex justify-between  w-full gap-4 md:gap-5 lg:gap-10">
                         <div className="flex flex-wrap w-max h-max gap-4">
                             <Image width={100} height={100} src="/icons/dss-company.svg" alt="" />
                             <Image width={100} height={100} src="/icons/iso.svg" alt="" />
                             <Image width={100} height={100} src="/icons/fintechnnr.svg" alt="" />
                         </div>
 
-                        <div className="grid grid-cols-2 gap-8 md:gap-20 px-3 md:px-0 py-6 lg:py-8 md:grid-cols-3 whitespace-nowrap">
+                        <div className="grid grid-cols-2 gap-3 px-3 md:px-0 py-6 lg:py-8 md:grid-cols-3 whitespace-nowrap">
                             <div>
                                 <h2 className="mb-3 md:mb-6 text-xs md:text-sm font-extrabold text-[#2A2A29] uppercase leading-[25.2px] tracking-[-2%]">Company</h2>
                                 <ul className="text-[#2A2A29] font-normal text-sm md:text-base leading-[17.64px] md:leading-[20.16px] tracking-[-1%] space-y-4 md:space-y-3">
